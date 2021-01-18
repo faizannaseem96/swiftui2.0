014c8918c3a8dd54aeb5f3686b759205dce74a01
Faizan  Naseem <faizannaseem@Faizans-MacBook-Pro.local>
2021-01-18 10:35:22 +0500

    project created

---
+++ swiftui2.0/swiftui2_0App.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui2_0App.swift
+//  swiftui2.0
+//
+//  Created by Faizan  Naseem on 18/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct swiftui2_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
