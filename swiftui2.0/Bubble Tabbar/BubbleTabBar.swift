e78e927dd53055fa02f7438d93b8c1ccf465704c
Faizan  Naseem <faizannaseem@Faizans-MacBook-Pro.local>
2021-01-18 11:57:20 +0500

    updated views

---
+++ swiftui2.0/Bubble Tabbar/BubbleTabBar.swift
@@ -0,0 +1,122 @@
+//
+//  BubbleTabBar.swift
+//  swiftui2.0
+//
+//  Created by Faizan  Naseem on 18/01/2021.
+//
+
+import SwiftUI
+
+struct BubbleTabBar: View {
+    
+    var tabs = ["home", "notifications", "search"]
+    
+    @State var selectedTab = "home"
+    
+    init() {
+        UITabBar.appearance().isHidden = true
+    }
+    
+    // Location of each curve
+    @State var xAxis: CGFloat = 0
+    @Namespace var animation
+    
+    var body: some View {
+        ZStack(alignment: Alignment(horizontal: .center, vertical: .bottom)) {
+            TabView(selection: $selectedTab) {
+                Color(.red)
+                    .ignoresSafeArea(.all, edges: .all)
+                    .tag("home")
+                
+                Color(.yellow)
+                    .ignoresSafeArea(.all, edges: .all)
+                    .tag("notifications")
+                
+                Color(.green)
+                    .ignoresSafeArea(.all, edges: .all)
+                    .tag("search")
+            }
+            
+            // custom tab bar
+            HStack(spacing: 0) {
+                ForEach(tabs, id: \.self) { image in
+                    GeometryReader { reader in
+                        Button(action: {
+                            withAnimation {
+                                selectedTab = image
+                                xAxis = reader.frame(in: .global).minX
+                            }
+                        }, label: {
+                            Image(image)
+                                .resizable()
+                                .renderingMode(.template)
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 22.0, height: 22.0)
+                                .foregroundColor(selectedTab == image ? .blue : .gray)
+                                .padding(selectedTab == image ? 15 : 0)
+                                .background(Color.white.opacity(selectedTab == image ? 1 : 0).clipShape(Circle()))
+                                .matchedGeometryEffect(id: image, in: animation)
+                                .offset(x: selectedTab == image ? (reader.frame(in: .global).minX - reader.frame(in: .global).midX) : 0, y: selectedTab == image ? -50 : 0)
+                        })
+                        .onAppear(perform: {
+                            if image == tabs.first {
+                                xAxis = reader.frame(in: .global).minX
+                                
+                            }
+                        })
+                    }
+                    .frame(width: 25.0, height: 30.0)
+                    if image != tabs.last { Spacer() }
+                }
+            }
+            .padding(.horizontal, 30)
+            .padding(.vertical)
+            .background(Color.white.clipShape(CustomShape(xAxis: xAxis)).cornerRadius(12.0))
+            .padding(.horizontal)
+            // Bottom edge....
+            .padding(.bottom , UIApplication.shared.windows.first?.safeAreaInsets.bottom)
+        }
+        .ignoresSafeArea(.all, edges: .all)
+    }
+}
+
+struct BubbleTabBar_Previews: PreviewProvider {
+    static var previews: some View {
+        BubbleTabBar()
+    }
+}
+
+struct CustomShape: Shape {
+    
+    var xAxis: CGFloat
+    
+    // Animate Path
+    var animatableData: CGFloat {
+        get { return xAxis }
+        set { xAxis = newValue }
+    }
+    
+    func path(in rect: CGRect) -> Path {
+        return Path { path in
+            path.move(to: CGPoint(x: 0, y: 0))
+            path.addLine(to: CGPoint(x: rect.width, y: 0))
+            path.addLine(to: CGPoint(x: rect.width, y: rect.height))
+            path.addLine(to: CGPoint(x: 0, y: rect.height))
+            
+            let center = xAxis
+            
+            path.move(to: CGPoint(x: center - 50, y: 0))
+            
+            let to1 = CGPoint(x: center, y: 35)
+            let control1 = CGPoint(x: center - 25, y: 0)
+            let control2 = CGPoint(x: center - 25, y: 35)
+            
+            let to2 = CGPoint(x: center + 50, y: 0)
+            let control3 = CGPoint(x: center + 25, y: 35)
+            let control4 = CGPoint(x: center + 25, y: 0)
+            
+            path.addCurve(to: to1, control1: control1, control2: control2)
+            path.addCurve(to: to2, control1: control3, control2: control4)
+        }
+    }
+}
